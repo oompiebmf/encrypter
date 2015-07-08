74a4c9a4dc0c5833de9ce0911d5aa91590e0ca61
oompiebmf <Solaris10>
2015-07-08 12:07:43 +0100

    1

--- init.rb
+++ init.rb
@@ -1,4 +1,25 @@
+require 'optparse'
 
+
+
+options = {}
+optparse = OptionParser.new do |opts|
+  opts.banner = "Usage: encrypter.rb -t type of encryption 1 = default 2 = non-default -s string to encrypt"
+  opts.on("-t type" "--type") do |type|
+    unless type == 1||2
+      raise ArgumentError, "The type must be either 1 or 2, chop"
+    end
+    options[:type] = type
+  end
+  opts.on('-s strng' '--string') do |strng|
+    unless strng
+      raise ArgumentError, "Please include a string chop"
+    end
+    options[:strng] = strng
+  end
+end
+
+optparse.parse!
 
 APP_ROOT = File.dirname(__FILE__)
 
